2a05b52c8bf16a2374e6f9a1c91cccb2bdb7a88f
Elvis Gastelum <elvisgastelum@outlook.com>
2019-10-02 12:32:15 -0700

    Add cors and fix static uploads folder

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
+import cors from 'cors';
 
 const app = express();
 
@@ -12,14 +13,14 @@ app.set('port', process.env.PORT || 4000);
 // middlewares
 app.use(morgan('dev'));
 app.use(express.json());
-
+app.use(cors());
 // routes
 app.use('/api', indexRoutes);
 
 
 
 // this folder for this application will be used to store public files
-app.use('uploads', express.static(path.resolve('uploads')));
+app.use('/uploads', express.static(path.resolve('uploads')));
 
 
 
